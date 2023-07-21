14941478ee953018227beab73928511e88b810dc
Suraj0326 <surajpersonal0327@gmail.com>
2023-07-21 14:44:09 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,81 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/footer";
+import About from "./components/About/about";
+import Projects from "./components/projects/projects";
+import Contact from "./components/Contact/contact";
+import Login from "./components/login/login";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, loadUser } from "./actions/user";
+import AdminPanel from "./components/Admin/adminPanel";
+import Timeline from "./components/Admin/timeline";
+import Youtube from "./components/Admin/Youtube";
+import Project from "./components/Admin/Project";
+import Loader from "./components/loader/loader";
+import Main from "./components/Main/Main";
+
+function App() {
+  const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.login);
+  const { loading, user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(loadUser());
+  }, [dispatch]);
+
+  return (
+    <Router>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Main
+                  youtubes={user.youtube}
+                  timelines={user.timeline}
+                  skills={user.skills}
+                />
+              }
+            />
+            <Route path="/about" element={<About about={user.about} />} />
+            <Route
+              path="/projects"
+              element={<Projects projects={user.projects} />}
+            />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/account"
+              element={isAuthenticated ? <AdminPanel /> : <Login />}
+            />
+            <Route
+              path="/admin/timeline"
+              element={isAuthenticated ? <Timeline /> : <Login />}
+            />
+            <Route
+              path="/admin/youtube"
+              element={isAuthenticated ? <Youtube /> : <Login />}
+            />
+
+            <Route
+              path="/admin/project"
+              element={isAuthenticated ? <Project /> : <Login />}
+            />
+          </Routes>
+
+          <Footer />
+        </>
+      )}
+    </Router>
+  );
+}
+
+export default App;
